725bab982e17600443a62984b3e5f05d11201ada
yagnik <yagnikpankhaniya45@gamil.com>
2025-10-13 21:07:05 +0530

    api

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,29 +1,24 @@
-// shivengroup-frontend/src/app/api/auth/[...nextauth]/route.js
-export const dynamic = "force-dynamic"
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import connectMongo from "@/lib/mongodb";
-import Admin from "@/models/Admin";
+import User from "@/models/User";
 import bcrypt from "bcryptjs";
-;
 
 const handler = NextAuth({
   providers: [
     CredentialsProvider({
-      name: "Admin Credentials",
+      name: "Credentials",
       credentials: {
         email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
         await connectMongo();
-        const admin = await Admin.findOne({ email: credentials.email });
-        if (!admin) return null;
-
-        const isValid = await bcrypt.compare(credentials.password, admin.password);
-        if (!isValid) return null;
-
-        return { id: admin._id, name: admin.name, email: admin.email };
+        const user = await User.findOne({ email: credentials.email });
+        if (!user) throw new Error("No user found with this email");
+        const isValid = await bcrypt.compare(credentials.password, user.password);
+        if (!isValid) throw new Error("Password is incorrect");
+        return { id: user._id, name: user.name, email: user.email };
       },
     }),
   ],
@@ -38,9 +33,6 @@ const handler = NextAuth({
       session.user = token.user;
       return session;
     },
-  },
-  pages: {
-    signIn: "/admin/login", // your custom admin login page
   },
 });
 
